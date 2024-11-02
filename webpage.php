ef610972ff927dfb549c5af1a09c6af8ac1d8700
Dell <Dell@DESKTOP-6S377EM>
2024-11-02 22:10:33 +0530

    20241102 html

---
+++ webpage.php
@@ -0,0 +1,18 @@
+<html>
+    
+    <head>
+        <title>New Web page</title>
+    </head>
+    <body>
+        <?php
+        echo "Hello World";
+        
+        ?>
+    </body>
+    <script>
+        fuction {
+            new:js;
+        }
+        </script>
+    
+</html>
